d72ad754ea93c010cb802e8a9f5102114ea20e37
denfad <denfad2003@mail.ru>
2024-03-30 15:41:41 +0300

    Add README

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ parser = ArgumentParser(
     auto_env_var_prefix='MOCK_',
     formatter_class=ArgumentDefaultsHelpFormatter
 )
-parser.add_argument('--api-address', default='0.0.0.0',
+parser.add_argument('--api-host', default='0.0.0.0',
                     help='IPv4/IPv6 address API server would listen on')
 parser.add_argument('--api-port', type=int, default=8080,
                     help='TCP port API server would listen on')
@@ -25,4 +25,4 @@ app = web.Application()
 stupid = Stupid(server_number)
 app.router.add_route('GET', '/hello', stupid.hello)
 
-web.run_app(app, host=args.api_address, port=args.api_port)
+web.run_app(app, host=args.api_host, port=args.api_port)
